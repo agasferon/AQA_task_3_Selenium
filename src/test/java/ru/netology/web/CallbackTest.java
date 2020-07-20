b5d4667ab950e4988a50a15e5a6c7dc3addd150e
Anatoly Malyshev <anatoliym67@gmail.com>
2020-07-20 13:04:18 +0300

    back change build.gradle

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -18,8 +18,8 @@ class CallbackTest {
 
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
-//        System.setProperty("webdriver.chrome.driver", "./driver/linux/chromedriver.exe");
+//        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "./driver/linux/chromedriver.exe");
     }
 
     @BeforeEach
