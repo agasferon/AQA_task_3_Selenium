e544c633953852c1da3b891532b316ae050d41fb
Anatoly Malyshev <anatoliym67@gmail.com>
2020-07-22 13:00:37 +0300

    fix & add WebDriverManager

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -1,25 +1,24 @@
 package ru.netology.web;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class CallbackTest {
     private WebDriver driver;
 
     @BeforeAll
     static void setUpAll() {
-//        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
-        System.setProperty("webdriver.chrome.driver", "./driver/linux/chromedriver");
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
@@ -35,18 +34,6 @@ class CallbackTest {
     void tearDown() {
         driver.quit();
         driver = null;
-    }
-
-    @Test
-    void shouldSearchByClassName() {
-        driver.get("http://localhost:9999");
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Иванов Иван");
-        elements.get(1).sendKeys("+79871234567");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
-        assertEquals("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
     }
 
     @Test
@@ -85,7 +72,7 @@ class CallbackTest {
     @Test
     void shouldNotSendWithInvalidPhoneNumber() {
         driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Алексей");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Иван");
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+798712345678");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         driver.findElement(By.cssSelector("button[type='button']")).click();
@@ -96,7 +83,7 @@ class CallbackTest {
     @Test
     void shouldNotSendWithLettersInsteadOfNumbers() {
         driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Алексей");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Иван");
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("Плюс Семь Девятьсот Пять");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         driver.findElement(By.cssSelector("button[type='button']")).click();
@@ -107,10 +94,11 @@ class CallbackTest {
     @Test
     void shouldNotSendWithoutCheckboxCheckColor() {
         driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Алексей");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Иван");
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79871234567");
+        String previousColor = driver.findElement(By.cssSelector("[data-test-id=agreement]")).getCssValue("color");
         driver.findElement(By.cssSelector("button[type='button']")).click();
-        String text = driver.findElement(By.cssSelector(".input_invalid")).getCssValue("color");
-        assertEquals("rgba(255, 92, 92, 1)", text);
+        String invalidColor = driver.findElement(By.cssSelector("[data-test-id=agreement].input_invalid")).getCssValue("color");
+        assertNotEquals(previousColor, invalidColor);
     }
 }
